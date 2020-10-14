3cb5852a4c78df0cfbeb9a67436c5ce28b8f9529
Istvan Szilagyi <istvan.szilagyi@statistik.bayern.de>
2020-10-14 09:23:40 +0200

    adapt to intellij

--- MiscJavaEEWeb/src/org/isz/miscjee/servlet/TestStatelessServlet.java
+++ MiscJavaEEWeb/src/org/isz/miscjee/servlet/TestStatelessServlet.java
@@ -2,22 +2,13 @@ package org.isz.miscjee.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Random;
 
-import javax.ejb.EJB;
-import javax.ejb.EJBLocalHome;
-import javax.ejb.EJBLocalObject;
-import javax.ejb.EJBObject;
-import javax.ejb.RemoveException;
 import javax.inject.Inject;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.test.ejbs.StatefulEJB;
-import org.test.ejbs.StatelessEJB;
 
 /**
  * Test stateless EJB.
@@ -40,13 +31,16 @@ public class TestStatelessServlet extends HttpServlet {
 	protected RequestScopedController aController; 
 	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
 		resp.setContentType("text/plain");
-		PrintWriter writer = resp.getWriter();
-		writer
-		.append("Served at: ").append(req.getContextPath()).append(CRLF);
-		aController.loopCalls(writer);
-	
-		
+		try
+		{
+			final PrintWriter	writer = resp.getWriter();
+			writer.append("Served at: ").append(req.getContextPath()).append(CRLF);
+			aController.loopCalls(writer);
+		} catch (IOException pE)
+		{
+			pE.printStackTrace();
+		}
 	}
 }
